8ef654ee00088e2aaacd40d12b72a731c5f77115
Cristiano Ferreira <cristiano.oliveira2@fatec.sp.gov.br>
2018-07-09 01:45:16 -0300

    Screen adjustment; Unity and Instrumented tests

--- app/src/androidTest/java/com/arctouch/codechallenge/BasicInstrumentedTest.java
+++ app/src/androidTest/java/com/arctouch/codechallenge/BasicInstrumentedTest.java
@@ -1,8 +1,11 @@
 package com.arctouch.codechallenge;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.arctouch.codechallenge.model.Movie;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +18,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class BasicInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void movieSerializationTest() throws Exception {
+        Bundle b = new Bundle();
+        Movie m1 = new Movie();
+        m1.id = 123;
+        m1.overview="blabla";
 
-        assertEquals("com.arctouch.codechallenge", appContext.getPackageName());
+        b.putSerializable("M",m1);
+
+        Movie m2 = (Movie)b.getSerializable("M");
+        assertEquals(m2, m1);
     }
 }
