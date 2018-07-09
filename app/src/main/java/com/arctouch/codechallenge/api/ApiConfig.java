8253cfb12d5df798c11031a05de818f75555ed88
Cristiano Ferreira <cristiano.oliveira2@fatec.sp.gov.br>
2018-07-08 23:29:59 -0300

    choosing a region with more movies

--- app/src/main/java/com/arctouch/codechallenge/api/ApiConfig.java
+++ app/src/main/java/com/arctouch/codechallenge/api/ApiConfig.java
@@ -3,6 +3,6 @@ package com.arctouch.codechallenge.api;
 public class ApiConfig {
     public static String URL = "https://api.themoviedb.org/3/";
     public static String API_KEY = "1f54bd990f1cdfb230adb312546d765d";
-    public static String DEFAULT_LANGUAGE = "pt-BR";
-    public static String DEFAULT_REGION = "BR";
+    public static String DEFAULT_LANGUAGE = "en-US";
+    public static String DEFAULT_REGION = "US";
 }
